27ccabbf0373412d76e6b9f5256080e3d6ecc097
Oscar <oscar_d305@github.com>
2018-05-21 16:19:31 -0500

    Contact feature finished plus counter

--- web/js/tabs.js
+++ web/js/tabs.js
@@ -22,3 +22,14 @@ function closeCity(evt, cityName){
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 }
+
+function textCounter(field,field2,maxlimit)
+{
+ var countfield = document.getElementById(field2);
+ if ( field.value.length > maxlimit ) {
+  field.value = field.value.substring( 0, maxlimit );
+  return false;
+ } else {
+  countfield.value = maxlimit - field.value.length;
+ }
+}
